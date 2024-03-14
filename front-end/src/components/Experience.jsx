b0ea9bf28e2f0a6b57bebf32fb3148d6a1aa4a0e
Eric <ericmitchell1625@hotmail.com>
2024-03-14 15:36:49 -0400

    css  added for experience and projects sections

--- front-end/src/components/Experience.jsx
+++ front-end/src/components/Experience.jsx
@@ -2,35 +2,35 @@ const Experience = () => {
   return (
     <>
     <div id='experience' className='space-y-[20px] mt-20'>
-    <h1>Experience / Education</h1>
+    <h1 className="text-3xl">Experience / Education</h1>
     <section style={{border:'black solid 1px'}}>
-    <div style={{display:'flex'}}>
-        <div>
-          <h2>Code Platoon - Student</h2>
-          <h2>October 2023 - January 2024</h2>
-          <p>Full Stack Software Engineering bootcamp training in Javascript, Python, Django, Django-REST-framework, SQL, and PostgreSQL.</p>
+      <div>
+        <div className="flex justify-between">
+        <h2 className="text-xl font-semibold">Code Platoon - Student</h2>
+        <h2 className="">October 2023 - January 2024</h2>
         </div>
-    </div>
+        <p>Full Stack Software Engineering bootcamp training in Javascript, Python, Django, Django-REST-framework, SQL, and PostgreSQL.</p>
+      </div>
     </section>
 
     <section style={{border:'black solid 1px'}}>
-    <div style={{display:'flex'}}>
-        <div>
-          <h2>Yonsei Korean Language Institute</h2>
+      <div>
+      <div className="flex justify-between">
+          <h2 className="text-xl font-semibold">Yonsei Korean Language Institute</h2>
           <h2>March 2021 - February 2023</h2>
+          </div>
           <p>2 year immersive Korean language program located at Yonsei University in Seoul, South Korea.</p>
         </div>
-    </div>
     </section>
 
     <section style={{border:'black solid 1px'}}>
-    <div style={{display:'flex'}}>
-        <div>
-          <h2>Florida State University</h2>
-          <h2>August 2010 - May 2014</h2>
-          <p>{"Bachelor's of Science"}</p>
-        </div>
-    </div>
+      <div>
+        <div className="flex justify-between">
+            <h2 className="text-xl font-semibold">Florida State University</h2>
+            <h2>August 2010 - May 2014</h2>
+          </div>
+            <p>{"Bachelor's of Science in Exercise Science"}</p>
+      </div>
     </section>
     </div>
     </>
--- front-end/src/components/Projects.jsx
+++ front-end/src/components/Projects.jsx
@@ -1,20 +1,17 @@
 const Projects = () => {
   return (
     <>
-    <div id='projects' className='space-y-[35px] mt-20 '>
+    <div id='projects' className='grid space-y-[35px] w-full lg:col-span-2 gap-[15px]'>
       <h1 className="text-3xl">Projects</h1>
-      <section className='project'>
-        <div className="flex">
-          <p className="" style={{backgroundColor:'black', width: '200px'}}></p>
+      <section className="p-3">
           <div>
             <h2 className="text-xl font-semibold">Tiki Taka</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ipsam esse placeat, doloremque, in tenetur similique est animi rerum odio vel quas, dolorem iste eos earum vero iure enim quasi!</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi quis cupiditate, harum saepe minus voluptas sequi minima nam exercitationem vel necessitatibus tempora animi cumque dolorem fugit aut beatae, optio officia.</p>
-          </div>
         </div>
       </section>
       
-      <section style={{border:'red solid 2px'}}>
+      <section className="p-3">
         <div style={{display:'flex'}}>
           <p style={{backgroundColor:'black', width: '200px'}}>faw</p>
           <div>
