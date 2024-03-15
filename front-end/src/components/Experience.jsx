1bb514dfffb09ee453187604d7f4cf9f7d51df62
eric <ericmitchell1625@hotmail.com>
2024-03-15 15:33:52 -0400

    responsiveness added for experience/education dates

--- front-end/src/components/Experience.jsx
+++ front-end/src/components/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
       <div>
         <div className="flex justify-between">
         <h2 className="text-2xl">Code Platoon</h2>
-        <h2 className="text-lg">October 2023 - January 2024</h2>
+        <h2 className="lg:text-lg md:text-sm">October 2023 - January 2024</h2>
         </div>
         <p className="text-lg">Full Stack Software Engineering bootcamp training in Javascript, Python, Django, Django-REST-framework, SQL, and PostgreSQL.</p>
       </div>
@@ -16,7 +16,7 @@ const Experience = () => {
     <section className="p-3">
       <div className="flex justify-between">
             <h2 className="text-2xl">Yonsei Korean Language Institute</h2>
-            <h2 className="text-lg">March 2021 - February 2023</h2>
+            <h2 className="lg:text-lg md:text-sm">March 2021 - February 2023</h2>
           </div>
           <p className="text-lg">2 year immersive Korean language program located at Yonsei University in Seoul, South Korea.</p>
     </section>
@@ -24,11 +24,11 @@ const Experience = () => {
     <section className="p-3">
       <div>
         <div className="flex justify-between">
-            <h2 className="text-2xl">Licesned Pracitcal Nurse - United States Army</h2>
-            <h2 className="text-lg">February 2015 - February 2021</h2>
+            <h2 className="text-2xl">United States Army</h2>
+            <h2 className="lg:text-lg md:text-sm">February 2015 - February 2021</h2>
           </div>
             <p className="text-lg">Provided medical care to Soldiers, retirees, and their families under the supervision of a physician or registered nurse.</p>
-            <p className="text-lg">Texas State Board of Nursing - License #331768</p>
+            <p className="text-lg">Texas State Board of Nursing - #331768</p>
       </div>
     </section>
 
@@ -36,7 +36,7 @@ const Experience = () => {
       <div>
         <div className="flex justify-between">
             <h2 className="text-2xl">Florida State University</h2>
-            <h2 className="text-lg">August 2010 - May 2014</h2>
+            <h2 className="lg:text-lg md:text-sm">August 2010 - May 2014</h2>
           </div>
             <p className="text-lg">{"Bachelor's of Science in Exercise Science"}</p>
       </div>
