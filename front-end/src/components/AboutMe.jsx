21a334ee23b6d6b83bfb8c9630f52a8cdd85d02f
eric <ericmitchell1625@hotmail.com>
2024-03-15 17:06:06 -0400

    css responsive

--- front-end/src/components/AboutMe.jsx
+++ front-end/src/components/AboutMe.jsx
@@ -7,11 +7,11 @@ const AboutMe = () => {
 			<>
 				<div id='aboutme' className="gap-[15px]">
           <div className="col-span-2"> 
-            <h1 className="text-4xl">Hello.</h1>
+            <h1 className="text-5xl">Hello.</h1>
             <h1 className="text-4xl">My name is Eric Mitchell.</h1>
           </div>
           <div className="p-3 text-lg">
-            <p>I spent six years in the Army working as a Licensed Practical Nurse. After finishing my contract, I wanted to spend some time aboard and study Korean in South Korea. I came back to the United States in 2023 to pursue a career in software development.</p>
+            <p>I spent six years in the Army working as a Licensed Practical Nurse. After finishing my contract, I wanted to spend some time aboard and decided to studied Korean in South Korea for two years. I came back to the United States in 2023 to pursue a career in software development.</p>
             <p>{"I'm actively seeking new opportunities for work. Feel free to connect with me."}</p>
           </div>
           <div className="flex justify-center gap-3">
--- front-end/src/components/Experience.jsx
+++ front-end/src/components/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
       <div>
         <div className="flex justify-between">
         <h2 className="text-2xl">Code Platoon</h2>
-        <h2 className="lg:text-lg md:text-sm">October 2023 - January 2024</h2>
+        <h2 className="lg:text-lg md:text-sm self-end">October 2023 - January 2024</h2>
         </div>
         <p className="text-lg">Full Stack Software Engineering bootcamp training in Javascript, Python, Django, Django-REST-framework, SQL, and PostgreSQL.</p>
       </div>
@@ -16,7 +16,7 @@ const Experience = () => {
     <section className="p-3">
       <div className="flex justify-between">
             <h2 className="text-2xl">Yonsei Korean Language Institute</h2>
-            <h2 className="lg:text-lg md:text-sm">March 2021 - February 2023</h2>
+            <h2 className="lg:text-lg md:text-sm self-end">March 2021 - February 2023</h2>
           </div>
           <p className="text-lg">2 year immersive Korean language program located at Yonsei University in Seoul, South Korea.</p>
     </section>
@@ -25,7 +25,7 @@ const Experience = () => {
       <div>
         <div className="flex justify-between">
             <h2 className="text-2xl">United States Army</h2>
-            <h2 className="lg:text-lg md:text-sm">February 2015 - February 2021</h2>
+            <h2 className="lg:text-lg md:text-sm self-end">February 2015 - February 2021</h2>
           </div>
             <p className="text-lg">Provided medical care to Soldiers, retirees, and their families under the supervision of a physician or registered nurse.</p>
             <p className="text-lg">Texas State Board of Nursing - #331768</p>
