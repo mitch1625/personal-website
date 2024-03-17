51c3a316ae7c8350b5aa65b52c0fa6a40f2098da
Eric <ericmitchell1625@hotmail.com>
2024-03-17 10:09:21 -0400

    scrollbar delay fixed by setting smooth=false

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -4,15 +4,15 @@ const NavBar = () => {
 
     return (
         <>
-        <div id='navbar-items' className=' text-xl p-2'>
+        <div id='navbar-items' className='text-xl p-2'>
           <ul >
-          <Link to='aboutme' activeClass='active' smooth={true} duration={600} offset={-100}>
+          <Link to='aboutme' activeClass='active' smooth={false} duration={600} offset={-100}>
               <li className='hover:scale-105'>About Me</li>
             </Link>
-            <Link to='projects' activeClass='active' smooth={true} duration={600} offset={-50}>
+            <Link to='projects' activeClass='active' smooth={false} duration={600} offset={-50}>
               <li className='hover:scale-105'>Projects</li>
             </Link>
-            <Link to='experience' activeClass='active' smooth={true} duration={600} offset={-50}>
+            <Link to='experience' activeClass='active' smooth={false} duration={600} offset={-50}>
               <li className='hover:scale-105'>Experience</li>
             </Link> 
           </ul>
