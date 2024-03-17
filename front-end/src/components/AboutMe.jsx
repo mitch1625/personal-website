89bac6abb584075f9aecf4969fa175baf6609199
Eric <ericmitchell1625@hotmail.com>
2024-03-16 20:56:54 -0400

    about me spacing added

--- front-end/src/components/AboutMe.jsx
+++ front-end/src/components/AboutMe.jsx
@@ -10,11 +10,8 @@ const AboutMe = () => {
             <h1 className="text-9xl -mx-1.5">Hello</h1>
             <h1 className="text-6xl">My name is Eric Mitchell.</h1>
           </div>
-          <div className="p-3 text-xl">
-            <p>I spent six years in the Army working as a licensed practical nurse. After finishing my contract, I wanted to spend some time aboard and decided to study Korean in South Korea for two years. I came back to the United States in 2023 to pursue a career in software development.</p>
-            <p>{"Currently I'm learning Typescript."}</p>
-            <p>{"I'm actively seeking new opportunities for work. Feel free to connect with me."}</p>
-          </div>
+            <p className="p-3 text-xl">I spent six years in the Army working as a licensed practical nurse. After finishing my contract, I wanted to spend some time aboard and decided to study Korean in South Korea for two years. I came back to the United States in 2023 to pursue a career in software development.</p>
+            <p className="p-3 text-xl">{"I'm actively seeking new opportunities for work. Feel free to connect with me."}</p>
           <div className="flex justify-end gap-3">
             <a href="https://www.github.com/mitch1625" target="blank">
               <img style={{width:'30px', height:'30px'}} src={github}/>
